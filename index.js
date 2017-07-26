a8b87bb2ad3c42aa74b3df84c26b2d63ed0bf379
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-26 21:08:07 +0700

    xong

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const data = require('./data');
 
 class Tin {
     constructor(title, desc, image, link) {
-
+        this.title = title;
+        this.image = image;
+        this.desc = desc;
+        this.link = link;
     }
 }
 
-const arrTin = [];
+const arrItems = data.split('<item>');
+arrItems.shift();
 
-function getBody(item, pre, post) {
-    // return  body
+const arrTin = arrItems.map(e => getNewsFromItem(e));
+console.log(arrTin);
+
+function getNewsFromItem(item) {
+    const title = getBody(item, '<title>', '</title>').trim();
+    const link = getBody(item, '<link>', '</link>').trim();
+    const image = getBody(item, 'src="', '" ></a>').trim();
+    const desc = getBody(item, '</a></br>', ']]>').trim();
+    return new Tin(title, desc, image, link);
 }
 
-/*
-    - Tach 25 
-    - 
-*/
+function getBody(item, pre, post) {
+    const startIndex = item.indexOf(pre) + pre.length;
+    const endIndex = item.indexOf(post);
+    return item.slice(startIndex, endIndex);
+}
+
+getNewsFromItem(arrItems[24]);
